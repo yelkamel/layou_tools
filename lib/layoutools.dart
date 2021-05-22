381ae0f30fe5162237a43ae3292e2b306ebc6841
Youcef <youcef@el-kamel.fr>
2021-05-22 17:19:43 +0200

    add first widget 😙

---
+++ lib/layoutools.dart
@@ -0,0 +1,7 @@
+library layoutools;
+
+import 'package:flutter/material.dart';
+
+part 'animation/entrace_fader.dart';
+part 'animation/loading_circle.dart';
+part 'animation/fadeintout_transitionner.dart';
