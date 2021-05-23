ac651768a263994a6d5179777598fe5bc8d782f8
Youcef <youcef@el-kamel.fr>
2021-05-23 18:02:33 +0200

    add wonderfull shadow position 🙃

--- lib/layoutools.dart
+++ lib/layoutools.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:intl/intl.dart';
 
+part 'extension.dart';
+
+part 'widget/shadow_mask.dart';
+
 part 'animation/entrace_fader.dart';
 part 'animation/loading_circle.dart';
 part 'animation/fadeintout_transitionner.dart';
-part 'extension.dart';
---
+++ lib/widget/shadow_mask.dart
@@ -0,0 +1,40 @@
+part of layoutools;
+
+enum ShadowPosition {
+  top,
+  bottom,
+}
+
+class ShadowMaskContainer extends StatelessWidget {
+  final Widget child;
+  final Color? color;
+  final ShadowPosition position;
+  const ShadowMaskContainer({
+    Key? key,
+    required this.child,
+    this.color,
+    this.position = ShadowPosition.top,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        if (position == ShadowPosition.top) child,
+        Container(
+          height: 1,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+              offset: Offset(0.0, 0.00),
+              blurRadius: 10.0,
+              color: color ?? Theme.of(context).backgroundColor,
+              spreadRadius: 12.00,
+            ),
+          ]),
+        ),
+        if (position == ShadowPosition.bottom) child,
+      ],
+    );
+  }
+}
