66f4581ba14c398aee00dfaaa9348fe7b4ad537d
Youcef <youcef@el-kamel.fr>
2021-05-23 17:32:15 +0200

    add magic extension with randomized int !   😊

---
+++ lib/extension.dart
@@ -0,0 +1,60 @@
+part of layoutools;
+
+extension LaYouList<T> on List<T> {
+  T get oneRandom {
+    if (length == 1) return this[0];
+    return this[Random().nextInt(length - 1)];
+  }
+}
+
+extension LaYouDuration on Duration {
+  String minuteSecondFormatted() {
+    return [
+      if (inSeconds > 3600) inHours.remainder(24),
+      inMinutes.remainder(60),
+      inSeconds.remainder(60)
+    ].map((seg) {
+      return seg.toString().padLeft(2, '0');
+    }).join(':');
+  }
+}
+
+extension LaYouInt on int {
+  String get minFormatFromSecondes => "${(this / 60).round().toString()} min";
+
+  bool get pourcentageOfChance => Random().nextInt(99) + 1 <= this;
+
+  int get randomFromZero {
+    return Random().nextInt(this);
+  }
+}
+
+extension LaYouDateTime on DateTime {
+  String get timeStringFromDate {
+    return DateFormat.Hm().format(this);
+  }
+
+  bool get isMorningTime {
+    final startTime = DateTime(year, month, day, 05, 59);
+    final endTime = DateTime(year, month, day, 12, 01);
+    return !(isAfter(startTime) && isBefore(endTime));
+  }
+
+  bool get isNightTime {
+    final startTime = DateTime(year, month, day, 18, 59);
+    final endTime = DateTime(year, month, day, 24, 01);
+    return !(isAfter(startTime) && isBefore(endTime));
+  }
+
+  bool get isToday {
+    if (this == null) return false;
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    final aDate = DateTime(year, month, day);
+    return aDate == today;
+  }
+
+  String get toDayMonth {
+    return DateFormat('d MMMM', 'fr_FR').format(this);
+  }
+}
--- lib/layoutools.dart
+++ lib/layoutools.dart
@@ -1,7 +1,10 @@
 library layoutools;
 
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:intl/intl.dart';
 
 part 'animation/entrace_fader.dart';
 part 'animation/loading_circle.dart';
 part 'animation/fadeintout_transitionner.dart';
+part 'extension.dart';
