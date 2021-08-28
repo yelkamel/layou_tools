1e2eeb443ebc0282c61a90a81e98bd297842b409
Youcef <youcef@el-kamel.fr>
2021-08-28 19:20:32 +0200

    Main button is to specific it's have been remove

--- lib/layoutools.dart
+++ lib/layoutools.dart
@@ -8,7 +8,6 @@ import 'package:simple_animations/simple_animations.dart';
 part 'extension.dart';
 
 part 'widget/shadow_mask.dart';
-part 'widget/main_button.dart';
 
 part 'animation/entrace_fader.dart';
 part 'animation/loading_circle.dart';
