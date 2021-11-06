66a3f73df974ab6b806257ba655dd2415b40e731
Youcef <youcef@el-kamel.fr>
2021-11-06 15:06:44 +0100

    better LIAD product model

--- lib/model/lindainartdesign/product.dart
+++ lib/model/lindainartdesign/product.dart
@@ -28,37 +28,34 @@ enum ProductStatus {
 class Product {
   String? id;
   String? email;
-  @JsonKey(toJson: productTypeToJson, fromJson: productTypeFromJson)
-  ProductType? type;
+  String type;
+  String backgroundType;
   String? descr;
   String? text;
   String? filename;
   int format;
   int nbOfPerson;
-  @JsonKey(toJson: backgroundTypeToJson, fromJson: backgroundTypeFromJson)
-  BackgroundType? backgroundType;
   @JsonKey(ignore: true)
   Uint8List? fileBytes;
   bool quick;
   @JsonKey(toJson: dateTimetoJson, fromJson: dateTimefromJson)
   DateTime? date;
-  @JsonKey(toJson: productStatusToJson, fromJson: productStatusFromJson)
-  ProductStatus? status;
+  String status;
 
   Product({
     this.id,
     this.email,
-    this.type = ProductType.portrait,
+    this.type = "portrait",
     this.descr,
     this.text,
     this.filename,
     this.format = 4,
     this.nbOfPerson = 1,
-    this.backgroundType = BackgroundType.united,
+    this.backgroundType = "united",
     this.fileBytes,
     this.quick = false,
     required this.date,
-    this.status = ProductStatus.notpay,
+    this.status = "notpay",
   });
 
   factory Product.fromJson(Map<String, dynamic> data, String docId) =>
@@ -68,13 +65,20 @@ class Product {
       });
 
   Map<String, dynamic> toJson() => _$ProductToJson(this);
-  @override
-  String toString() =>
-      '===> Product\n=> type:$type\n=> email:$email\n=> filename:$filename\n=> format:$format\n=> nbOfPerson:$nbOfPerson\n=> backgroundType:$backgroundType\n=> quick:$quick\n=> descr:$descr';
 
-/*
-https://lindainartdesign-merci.web.app/
-*/
+  @override
+  String toString() => toJson().toString();
+
+  bool get isPortrait => type == 'portrait';
+  bool get isPoster => type == 'poster';
+
+  bool get isUnited => type == 'united';
+  bool get isIllustrated => type == 'illustrated';
+
+  bool get isNotpay => type == 'notpay';
+  bool get isPayed => type == 'payed';
+  bool get isRefused => type == 'refused';
+
   String get getUrlStripePaymentLink {
     if (totalPrice == 45) return "https://buy.stripe.com/00gaFV2MwboI6kMeVh";
     if (totalPrice == 50) return "https://buy.stripe.com/fZedS7fzi8cwbF68wU";
@@ -97,75 +101,52 @@ https://lindainartdesign-merci.web.app/
     return "https://lindainartdesign-merci.web.app/";
   }
 
+  double calculateWithOptionPrice(int basePrice) =>
+      (isIllustrated ? 15 : 0) +
+      (format == 4 ? 5 : 0) +
+      (quick ? (basePrice * 25 / 100).round().toDouble() : 0);
+
   double get totalPrice {
-    if (nbOfPerson == 1) {
-      final basePrice = 57;
-      return basePrice +
-          (backgroundType == BackgroundType.illustrated ? 15 : 0) +
-          (format == 4 ? 10 : 0) +
-          (quick ? (basePrice * 25 / 100).round().toDouble() : 0);
-    }
+    if (nbOfPerson == 1) return calculateWithOptionPrice(45);
+    if (nbOfPerson == 2) return calculateWithOptionPrice(55);
+    if (nbOfPerson == 3) return calculateWithOptionPrice(65);
+    if (nbOfPerson == 4) return calculateWithOptionPrice(75);
+    if (nbOfPerson == 5) return calculateWithOptionPrice(85);
+    if (nbOfPerson == 6) return calculateWithOptionPrice(95);
 
-    if (nbOfPerson == 2) {
-      final basePrice = 77;
-      return basePrice +
-          (backgroundType == BackgroundType.illustrated ? 15 : 0) +
-          (format == 4 ? 10 : 0) +
-          (quick ? (basePrice * 25 / 100).round().toDouble() : 0);
-    }
-
-    if (nbOfPerson == 3) {
-      final basePrice = 97;
-      return basePrice +
-          (backgroundType == BackgroundType.illustrated ? 15 : 0) +
-          (format == 4 ? 10 : 0) +
-          (quick ? (basePrice * 25 / 100).round().toDouble() : 0);
-    }
-    if (nbOfPerson == 4) {
-      final basePrice = 117;
-      return basePrice +
-          (backgroundType == BackgroundType.illustrated ? 15 : 0) +
-          (format == 4 ? 10 : 0) +
-          (quick ? (basePrice * 25 / 100).round().toDouble() : 0);
-    }
-
-    final basePrice = 137;
-    return basePrice +
-        (backgroundType == BackgroundType.illustrated ? 15 : 0) +
-        (format == 4 ? 10 : 0) +
-        (quick ? (basePrice * 25 / 100).round().toDouble() : 0);
+    return 45.0;
   }
-}
 
-ProductType? productTypeFromJson(String? str) {
-  if (str == null) return null;
-  var enumObject = EnumObject<ProductType>(ProductType.values);
-  return enumObject.enumFromString(str);
-}
+  ProductType? productTypeFromJson(String? str) {
+    if (str == null) return null;
+    var enumObject = EnumObject<ProductType>(ProductType.values);
+    return enumObject.enumFromString(str);
+  }
 
-String? productTypeToJson(ProductType? type) {
-  if (type == null) return null;
-  return type.enumValue;
-}
+  String? productTypeToJson(ProductType? type) {
+    if (type == null) return null;
+    return type.enumValue;
+  }
 
-ProductStatus? productStatusFromJson(String? str) {
-  if (str == null) return null;
-  var enumObject = EnumObject<ProductStatus>(ProductStatus.values);
-  return enumObject.enumFromString(str);
-}
+  ProductStatus? productStatusFromJson(String? str) {
+    if (str == null) return null;
+    var enumObject = EnumObject<ProductStatus>(ProductStatus.values);
+    return enumObject.enumFromString(str);
+  }
 
-String? productStatusToJson(ProductStatus? type) {
-  if (type == null) return null;
-  return type.enumValue;
-}
+  String? productStatusToJson(ProductStatus? type) {
+    if (type == null) return null;
+    return type.enumValue;
+  }
 
-BackgroundType? backgroundTypeFromJson(String? str) {
-  if (str == null) return null;
-  var enumObject = EnumObject<BackgroundType>(ProductType.values);
-  return enumObject.enumFromString(str);
-}
+  BackgroundType? backgroundTypeFromJson(String? str) {
+    if (str == null) return null;
+    var enumObject = EnumObject<BackgroundType>(ProductType.values);
+    return enumObject.enumFromString(str);
+  }
 
-String? backgroundTypeToJson(BackgroundType? type) {
-  if (type == null) return null;
-  return type.enumValue;
+  String? backgroundTypeToJson(BackgroundType? type) {
+    if (type == null) return null;
+    return type.enumValue;
+  }
 }
--- lib/model/lindainartdesign/product.g.dart
+++ lib/model/lindainartdesign/product.g.dart
@@ -9,35 +9,29 @@ part of 'product.dart';
 Product _$ProductFromJson(Map<String, dynamic> json) => Product(
       id: json['id'] as String?,
       email: json['email'] as String?,
-      type: json['type'] == null
-          ? ProductType.portrait
-          : productTypeFromJson(json['type'] as String?),
+      type: json['type'] as String? ?? "portrait",
       descr: json['descr'] as String?,
       text: json['text'] as String?,
       filename: json['filename'] as String?,
       format: json['format'] as int? ?? 4,
       nbOfPerson: json['nbOfPerson'] as int? ?? 1,
-      backgroundType: json['backgroundType'] == null
-          ? BackgroundType.united
-          : backgroundTypeFromJson(json['backgroundType'] as String?),
+      backgroundType: json['backgroundType'] as String? ?? "united",
       quick: json['quick'] as bool? ?? false,
       date: dateTimefromJson(json['date'] as Timestamp?),
-      status: json['status'] == null
-          ? ProductStatus.notpay
-          : productStatusFromJson(json['status'] as String?),
+      status: json['status'] as String? ?? "notpay",
     );
 
 Map<String, dynamic> _$ProductToJson(Product instance) => <String, dynamic>{
       'id': instance.id,
       'email': instance.email,
-      'type': productTypeToJson(instance.type),
+      'type': instance.type,
+      'backgroundType': instance.backgroundType,
       'descr': instance.descr,
       'text': instance.text,
       'filename': instance.filename,
       'format': instance.format,
       'nbOfPerson': instance.nbOfPerson,
-      'backgroundType': backgroundTypeToJson(instance.backgroundType),
       'quick': instance.quick,
       'date': dateTimetoJson(instance.date),
-      'status': productStatusToJson(instance.status),
+      'status': instance.status,
     };
