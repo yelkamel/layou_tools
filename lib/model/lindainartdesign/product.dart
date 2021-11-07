75d1f61b64c053ba4c0a1e5e336bbb4f1b32fb3e
Youcef <youcef@el-kamel.fr>
2021-11-07 13:18:53 +0100

    ignore some field for pdocut

--- lib/model/lindainartdesign/product.dart
+++ lib/model/lindainartdesign/product.dart
@@ -12,10 +12,12 @@ class Product {
   String? email;
   String type;
   String backgroundType;
+  @JsonKey(ignore: true)
   int format;
   String? descr;
   String? text;
   String? filename;
+  @JsonKey(ignore: true)
   int nbOfPerson;
   @JsonKey(ignore: true)
   Uint8List? fileBytes;
