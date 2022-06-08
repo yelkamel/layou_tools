7f5695291685f0d7c13a70f762e2df16835c75f2
Youcef <yelkamel@gmail.com>
2022-06-08 16:27:22 +0200

    🔥 add lottie simple player

--- lib/layoutools.dart
+++ lib/layoutools.dart
@@ -1,10 +1,11 @@
 library layoutools;
 
-import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 import 'package:simple_animations/simple_animations.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
 
 part 'widget/shadow_mask.dart';
 
@@ -12,6 +13,7 @@ part 'animation/entrace_fader.dart';
 part 'animation/loading_circle.dart';
 part 'animation/fadeinout_transitionner.dart';
 part 'animation/gradient_animated.dart';
+part 'animation/lottie_animated.dart';
 
 part 'extension/datetime.dart';
 part 'extension/duration.dart';
