b01b62fe8429dd3beeea26cbc6d110cecd8a9763
Youcef <yelkamel@gmail.com>
2022-08-18 20:09:03 +0200

    🐛 fix oneRandom to get the last

--- lib/extension/list.dart
+++ lib/extension/list.dart
@@ -3,6 +3,6 @@ part of layoutools;
 extension LaYouList<T> on List<T> {
   T get oneRandom {
     if (length == 1) return this[0];
-    return this[Random().nextInt(length - 1)];
+    return this[Random().nextInt(length)];
   }
 }
